b66f85859d88e3b9620487742c26a29152195657
Andres Orellano <andresorellano591@gmail.com>
2024-09-15 14:04:45 -0500

    Agregar archivos de proyecto.

---
+++ README.md
@@ -0,0 +1,1 @@
+Kbsport3: Ropa deportiva de alta calidad para quienes viven el deporte. Encuentra comodidad, estilo y rendimiento en cada prenda
---
+++ Vista/Login.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Vista
+{
+    public partial class FormLogin : Form
+    {
+        public FormLogin()
+        {
+            InitializeComponent();
+        }
+
+        private void HorFecha_Tick(object sender, EventArgs e)
+        {
+            labelHora.Text = DateTime.Now.ToLongTimeString();
+            labelFecha.Text = DateTime.Now.ToLongDateString();
+        }
+    }
+}
