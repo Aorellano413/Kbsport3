2f6770c475b5b055fd56f053be9f0b0fdb9313ff
Andres Orellano <andresorellano591@gmail.com>
2024-10-23 21:35:29 -0500

    Nuevo Paquete

---
+++ Entidades/Factura.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Entidades
+{
+    public class Factura
+    {
+        private int idFactura;
+
+        public Factura(int idFactura, DateTime fechaFactura, string cedula)
+        {
+            this.idFactura = idFactura;
+            FechaFactura = fechaFactura;
+            Cedula = cedula;
+        }
+
+        public int Id { get; set; }
+        public DateTime FechaFactura { get; set; }
+        public List<Camisa> CamisasVendidas { get; set; }
+        public decimal Total { get; set; }
+        public object Cedula { get; set; }
+    }
+}
